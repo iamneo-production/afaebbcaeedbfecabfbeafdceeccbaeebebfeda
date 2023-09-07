3eb9049b23971a88f93c85ca2d8ee52b5213a555
Iamneo <techadmin@iamneo.ai>
2023-09-07 16:01:27 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms'; // Add FormsModule
 
 import { AppComponent } from './app.component';
 import { AppFoodListComponent } from './app-food-list/app-food-list.component';
-import { FoodFilterPipe } from './food-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppFoodListComponent,
-    FoodFilterPipe
+    AppFoodListComponent
   ],
   imports: [
     BrowserModule,
