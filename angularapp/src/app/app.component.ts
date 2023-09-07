632496742f119c3e765de3fdde947eefa2b0d9c1
Iamneo <techadmin@iamneo.ai>
2023-09-07 15:01:27 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,9 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   foods = [
-    { name: 'Apple', calories: 52, image: 'path/to/apple.jpg' },
-    { name: 'Banana', calories: 89, image: 'path/to/banana.jpg' },
-    // Add more food items as needed
+    { name: 'Banana', calories: 105, image: 'banana.jpg' },
+    { name: 'Salmon', calories: 367, image: 'salmon.jpg' },
+    { name: 'Cake', calories: 225, image: 'cake.jpg' }
   ];
-}
 
+  todayFoods = [];
+
+  searchText = '';
+
+  showAddForm = false;
+
+  newFood = {
+    name: '',
+    calories: 0,
+    image: '',
+    quantity: 1
+  };
+
+  addFood() {
+    if (this.newFood.name && this.newFood.calories && this.newFood.image) {
+      this.foods.push({ ...this.newFood });
+      this.newFood.name = '';
+      this.newFood.calories = 0;
+      this.newFood.image = '';
+      this.newFood.quantity = 1;
+      this.showAddForm = false;
+    }
+  }
+
+  addToToday(food) {
+    const existingFood = this.todayFoods.find(item => item.name === food.name);
+
+    if (existingFood) {
+      existingFood.quantity += food.quantity;
+    } else {
+      this.todayFoods.push({ ...food });
+    }
+  }
+
+  removeFromToday(food) {
+    const index = this.todayFoods.indexOf(food);
+    if (index !== -1) {
+      this.todayFoods.splice(index, 1);
+    }
+  }
+}
--- food-app/src/app/app.module.ts
+++ food-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodListComponent } from './food-list/food-list.component'; // Import the FoodListComponent
+import { FoodFilterPipe } from './food-filter.pipe'; // Import the FoodFilterPipe
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodListComponent, // Add FoodListComponent to declarations
+    FoodFilterPipe // Add FoodFilterPipe to declarations
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule // Add FormsModule to imports
   ],
   providers: [],
   bootstrap: [AppComponent]
