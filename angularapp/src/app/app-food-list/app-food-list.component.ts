af5f408642305dc061eb88473c2ad662552e763b
Iamneo <techadmin@iamneo.ai>
2023-09-07 16:01:48 +0000

    Backup Commit

--- angularapp/src/app/app-food-list/app-food-list.component.ts
+++ angularapp/src/app/app-food-list/app-food-list.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { Food } from '../food';
+import { foods } from '../food-list';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './app-food-list.component.html',
-  styleUrls: ['./app-food-list.component.css']
+  styleUrls: ['./app-food-list.component.css'],
 })
 export class AppFoodListComponent {
-  @Input() foods: any[];
-  @Input() searchTerm: string;
+  foods = foods;
+  todaysFoods: Food[] = [];
+
+  addToTodaysFoods(food: Food) {
+    this.todaysFoods.push(food);
+  }
+
+  getTotalCalories(): number {
+    return this.todaysFoods.reduce((total, food) => total + food.calories, 0);
+  }
 }
